1d029789950287c45a16c23005e50d27c6412816
Mini187 Cours2025 <ENGGm616@uottawa.ca>
2025-04-29 11:58:43 -0400

    second save

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
-
+// sesion 2
 let gravity = 0.25;
-let bird-dy = 
+let bird_dy = 0;
+let score = 0;
+let game_state= "start";
+
+
+
+
+
+// sesion 2
+let bird = decument. getElememtById("bird");
+let score_display = decument. getElememtById("score");
+let game_container = decument. getElememtById("game-container");
+let start_btn = decument. getElememtById("start-btn");
+
+function applyGravity() {
+    bird_dy += gravity;
+    let birdtop = bird.offsettop + bird_dy;
+    birdtop = Math.max(birdtop, 0);
+// iff (bird < 0) {birdtyop = o;}
+birdtop = Math.min(birdtop,game_container.offsetHeight - bird.offsetHeight);
+
+bird.style.top = birdtop + "px";
+}
