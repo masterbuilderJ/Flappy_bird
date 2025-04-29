16b9e4c17c12504e0aed1bee2ec4902fb7a3fee2
Mini187 Cours2025 <ENGGm616@uottawa.ca>
2025-04-29 14:26:11 -0400

    third

--- script.js
+++ script.js
@@ -2,24 +2,55 @@
 let gravity = 0.25;
 let bird_dy = 0;
 let score = 0;
-let game_state= "start";
+let game_state = "start";
 
-
-
-
+let gameInterval = null;
 
 // sesion 2
-let bird = decument. getElememtById("bird");
-let score_display = decument. getElememtById("score");
-let game_container = decument. getElememtById("game-container");
-let start_btn = decument. getElememtById("start-btn");
+let bird = document.getElementById("bird");
+let score_display = document.getElementById("score");
+let game_container = document.getElementById("game_container");
+let start_btn = document.getElementById("button");
 
 function applyGravity() {
-    bird_dy += gravity;
-    let birdtop = bird.offsettop + bird_dy;
-    birdtop = Math.max(birdtop, 0);
-// iff (bird < 0) {birdtyop = o;}
-birdtop = Math.min(birdtop,game_container.offsetHeight - bird.offsetHeight);
+  bird_dy += gravity;
+  let birdtop = bird.offsetTop + bird_dy;
 
-bird.style.top = birdtop + "px";
+  bird.classList.remove("jump");
+  bird.classList.remove("fall");
+  if (bird_dy < 0) {
+    bird.classList.add("jump");
+  } else if (bird_dy > 0) {
+    bird.classList.add("fall");
+  }
+  birdtop = Math.max(birdtop, 0);
+  birdtop = Math.min(birdtop, game_container.offsetHeight - bird.offsetHeight);
+
+  bird.style.top = birdtop + "px";
+}
+
+function startGame() {
+  if (gameInterval !== null) return;
+
+  gameInterval = setInterval(() => {
+    applyGravity();
+  }, 10);
+}
+
+document.addEventListener("keydown", (e) => {
+  if (e.code === "Space" || e.code === "ArrowUp") {
+    if (game_state !== "Play") {
+      game_state = "Play";
+      startGame();
+    }
+
+    bird_dy = -7;
+  }
+});
+
+function onStartButtonClick() {
+  if (game_state !== "Play") {
+    game_syate = "Play";
+    startGame();
+  }
 }
