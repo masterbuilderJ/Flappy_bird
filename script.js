e39f06dd59a5be7245a2e29acb4bb2d9dff0187b
Mini187 Cours2025 <ENGGm616@uottawa.ca>
2025-04-30 10:29:26 -0400

    pipes

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ let gravity = 0.25;
 let bird_dy = 0;
 let score = 0;
 let game_state = "start";
+let pipes = [];
+let pipe_gap = 250;
 
 let gameInterval = null;
 
@@ -53,4 +55,46 @@ function onStartButtonClick() {
     game_syate = "Play";
     startGame();
   }
+}
+
+
+// greate pipe
+function createpipe() {
+  let pipe_position =
+  math.floor(math.rabdom() * (game_container.offsetHeight - pipe_gap - 100)) + 
+  50;
+
+  // top pipe
+  let top_pipe = document.createElement("div");
+  top_pipe.className = "pipe";
+  top_pipe.style.height = pipe_position + "px";
+  top_pipe.style.top = "0px";
+  top_pipe.style.left = "100%";
+  game_container.appendChild(top_pipe);
+
+// bottom pipe
+let bottom_pipe = document.createElement("div");
+bottom_pipe.className = "pipe";
+bottom_pipe.style.height = 
+game_container.offsetHeight - pipe_gap - pipe_position + "px";
+bottom_pipe.style.bottom = "0px";
+bottom_pipe.style.left = "100%";
+game_container.appendChild(bottom_pipe);
+
+pipe_position.push(top_pipe, bottom_pipe);
+}
+
+// move pipes
+function movePipes() {
+  for (let pipe of pipe) {
+    pipe.style.left = pipe.offsetleft - 3 + "px";
+
+    //remove pipe off screan
+    if (pipe.offsetleft < -pipe.offsetWidth) {
+      pipe.remove();
+    }
+  }
+
+  //remove old pipes from the array
+  pipes = pipes.filter((pipe) => pipe.offsetleft + pipe.offsetWidth > 0);
 }
